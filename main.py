a133541b9a5a5f9e928087265c0672118d2103a8
alaa eddine guenat <Oa_geunat@esi.dz>
2026-01-09 11:57:58 +0100

    some db setups

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from app.router.user_router import router as user_router
+from app.utils.init_db import create_tables
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app : FastAPI):
+     # instialize DB at Start
+     create_tables()
+     yield # sepration point 
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(user_router)
 
 @app.get("/test")
